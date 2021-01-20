878ed1c08ee48b85cd6a4cf18d3a4e4e031e9079
Chelsea <chelsea.e.rolland@gmail.com>
2021-01-20 12:44:39 -0600

    still working on the rest person controller

--- src/main/java/com/backend/technical/controllers/RestPersonController.java
+++ src/main/java/com/backend/technical/controllers/RestPersonController.java
@@ -1,5 +1,22 @@
 package com.backend.technical.controllers;
 
+import com.backend.technical.modals.Person;
+import com.backend.technical.repos.PersonRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RestController
 public class RestPersonController {
+    private PersonRepository personDao;
+
+    public RestPersonController (PersonRepository personDao){
+        this.personDao = personDao;
+    }
+
+    @GetMapping("/persons")
+    public List<Person> viewPeople () {
+        return personDao.findAll();
+    }
 }
