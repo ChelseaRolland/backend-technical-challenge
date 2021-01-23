b0d430f3192a1ae13367710a0f007bbe37a81363
Chelsea <chelsea.e.rolland@gmail.com>
2021-01-22 22:18:26 -0600

    working on the rest api returning, running into issue with the spring security will figure out with looking other docs, create the find 1 and all as well as delete, will see if I can see it tomorrow when fixing spring security

--- src/main/java/com/backend/technical/controllers/RestPersonController.java
+++ src/main/java/com/backend/technical/controllers/RestPersonController.java
@@ -14,8 +14,25 @@ public class RestPersonController {
         this.personDao = personDao;
     }
 
+    //Find 1 person via id
    @RequestMapping(value = "/person/{id}", method = RequestMethod.GET, produces = {"application/json", "application/xml"})
     public Person findPerson(@PathVariable long id){
         return personDao.getOne(id);
    }
+    //Find all people
+    @RequestMapping(value = "/person/all", method = RequestMethod.GET, produces = {"application/json", "application/xml"})
+    public List<Person> findAllPeople(){
+        return personDao.findAll();
+    }
+
+    //add person
+
+
+    //update person
+
+    //delete person
+    @RequestMapping(value = "/person/delete/{id}", method = RequestMethod.DELETE, produces = {"application/json", "application/xml"})
+    public void deletePerson(@PathVariable long id){
+        personDao.deleteById(id);
+    }
 }
