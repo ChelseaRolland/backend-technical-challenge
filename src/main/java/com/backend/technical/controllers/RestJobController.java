40c68a09ab3a3926aa241d4bca7606ac56011b78
Chelsea <chelsea.e.rolland@gmail.com>
2021-01-27 18:36:27 -0600

    still working on the job rest controller

--- src/main/java/com/backend/technical/controllers/RestJobController.java
+++ src/main/java/com/backend/technical/controllers/RestJobController.java
@@ -4,12 +4,11 @@ import com.backend.technical.modals.Job;
 import com.backend.technical.repos.JobRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static java.lang.Long.parseLong;
 
 @RestController
 @RequestMapping(value = "/api/jobs")
@@ -28,10 +27,30 @@ public class RestJobController {
     }
 
     //Adding 1 job
+    @RequestMapping(value = "/jobs", method = RequestMethod.POST)
+    public ResponseEntity<Object> addJob(@RequestBody Job job){
+        jobDao.save(job);
+        return new ResponseEntity<>("Job was created successfully", HttpStatus.CREATED);
+    }
 
     //Find 1 job via id
+    @RequestMapping(value = "/job/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Object> showJob(@PathVariable("id") String id){
+        return new ResponseEntity<>(jobDao.findById(parseLong(id)), HttpStatus.OK);
+    }
 
     //update Job
+    @RequestMapping(value = "/job/{id}", method = RequestMethod.PATCH)
+    public ResponseEntity<Object> updatePerson(@PathVariable("id") String id, @RequestBody Job job){
+        job.setId(parseLong(id));
+        jobDao.save(job);
+        return new ResponseEntity<>("Job was updated successfully", HttpStatus.OK);
+    }
 
     //delete job
+    @RequestMapping(value = "/job/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Object> deletePerson(@PathVariable("id") String id) {
+        jobDao.deleteById(parseLong(id));
+        return new ResponseEntity<>("Job has been deleted successfully", HttpStatus.OK);
+    }
 }
